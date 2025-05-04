fb5c01023247a32649587f71c32aa2ff3fd3f533
Antokochumon <Antokochumon2@gmail.com>
2025-05-04 20:48:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
 import Footer from './Components/footer/footer'; // Import Footer
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <RowPost url={originals} title="Netflix Originals" />
-      <RowPost url={action} title="Action" isSmall />
-      <Footer /> {/* Include Footer */}
-    </div>
+    <BrowserRouter basename="/netflix-clone"> {/* Replace with your repository name */}
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <RowPost url={originals} title="Netflix Originals" />
+        <RowPost url={action} title="Action" isSmall />
+        <Footer /> {/* Include Footer */}
+      </div>
+    </BrowserRouter>
   );
 }
 
